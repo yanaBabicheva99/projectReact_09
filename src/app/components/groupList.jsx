b22679ed26469a41302ac71741600a8a46b440ce
yanaBabicheva99 <103054110+yanaBabicheva99@users.noreply.github.com>
2022-07-29 00:58:02 +0300

    change GroupList

--- src/app/components/groupList.jsx
+++ src/app/components/groupList.jsx
@@ -4,17 +4,28 @@ import PropTypes from 'prop-types';
 const GroupList = ({items, valueProperty, contentProperty, onItemSelect, selectedItem}) => {
     return (
         <ul className="list-group">
-            {items.map(item => (
-                <li
-                    style={{cursor: 'pointer'}}
-                    key={item[valueProperty]}
-                    className={'list-group-item' + (item === selectedItem ? ' active' : '')}
-                    onClick={() => onItemSelect(item)}
-                >
-                    {item[contentProperty]}
-                </li>
-            ))}
-
+            {Array.isArray(items)
+                ? items.map(item => (
+                    <li
+                        style={{cursor: 'pointer'}}
+                        key={item[valueProperty]}
+                        className={'list-group-item' + (item === selectedItem ? ' active' : '')}
+                        onClick={() => onItemSelect(item)}
+                    >
+                        {item[contentProperty]}
+                    </li>
+                ))
+                : Object.keys(items).map(item => (
+                    <li
+                        style={{cursor: 'pointer'}}
+                        key={items[item][valueProperty]}
+                        className={'list-group-item' + (items[item] === selectedItem ? ' active' : '')}
+                        onClick={() => onItemSelect(items[item])}
+                    >
+                        {items[item][contentProperty]}
+                    </li>
+                ))
+            }
         </ul>
     );
 };
